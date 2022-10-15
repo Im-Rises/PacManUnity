6398fce55e403b9e24b8f62ddcbef6858cabf48c
Im-Rises <quentin-morel88@hotmail.com>
2022-10-15 17:02:35 -0400

    Corrected controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
         public float speed = 10f;
-        public string colliderTag = "WallsBorders";
-        private CircleCollider2D _circleCollider;
+        public Tilemap tilemap;
+        public Vector2 _orignalDirection = new(-1, 0);
+        public float initPositionOffset = 0.5f;
+
+        private Vector2 _destination;
         private Vector2 _inputDirection;
         private Vector2 _lastDirection;
         private Vector2 _lastInput;
         private Rigidbody2D _rigidbody2D;
 
-        // public Vector2 step = new(1f, 1f);
-        // public float initPositionOffset = 0.5f;
-        private Vector2 _destination { get; set; }
-
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _destination = transform.position;
+            _destination = (Vector2)transform.position + _orignalDirection * initPositionOffset;
+            _lastDirection = _orignalDirection;
         }
 
         private void FixedUpdate()
         {
-            var positionVector = Vector2.MoveTowards(transform.position, _destination, speed * Time.deltaTime);
+            var position = transform.position;
+            var positionVector = Vector2.MoveTowards(position, _destination, speed * Time.deltaTime);
             _rigidbody2D.MovePosition(positionVector);
 
-            var isCentered = (Vector2)transform.position == _destination;
+            var isCentered = (Vector2)position == _destination;
+
+            if (!isCentered) return;
 
             // if is at the middle of a tile, has input and there is no wall in the direction of the input
-            if (isCentered && _lastInput != Vector2.zero && !DetectWallBorder(_lastInput))
+            if (_lastInput != Vector2.zero && !DetectWallBorder(_lastInput))
             {
                 _destination = (Vector2)transform.position + _lastInput;
                 _lastDirection = _lastInput;
             }
-            // if is at the middle of a tile and there is no wall in the current direction
-            else if (isCentered && !DetectWallBorder(_lastDirection))
+            // if is at the middle of a tile and there is no wall in the current direction then continue in the same direction
+            else if (!DetectWallBorder(_lastDirection))
             {
                 _destination = (Vector2)transform.position + _lastDirection;
             }
         }
 
-        private bool DetectWallBorder(Vector2 dir)
+        private bool DetectWallBorder(Vector3 dir)
         {
-            Vector2 pos = transform.position;
-            var centerHit = Physics2D.Linecast(pos + dir, pos);
-            // var temp = new Vector2(dir.y, dir.x);
-            var rearHit = Physics2D.Linecast(pos + dir + dir * _circleCollider.bounds.extents, pos);
-            return centerHit.collider.CompareTag(colliderTag) || rearHit.collider.CompareTag(colliderTag);
+            var pos = transform.position;
+            var cellPosition = tilemap.WorldToCell(pos + dir);
+            var linecast = Physics2D.Linecast(pos + dir, pos);
+            return tilemap.HasTile(cellPosition) || linecast.collider.CompareTag(tilemap.tag);
         }
 
         private void OnMove(InputValue value)
