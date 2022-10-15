2f06d88a43233a4c963152b653dc6c2446e65b5d
Im-Rises <quentin-morel88@hotmail.com>
2022-10-15 18:46:54 -0400

    Added animations and corrected Player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,44 +7,64 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         public float speed = 10f;
+
         public Tilemap tilemap;
-        public Vector2 _orignalDirection = new(-1, 0);
+        public Vector2 originalDirection = new(-1, 0);
         public float initPositionOffset = 0.5f;
 
-        private Vector2 _destination;
+        public Animator animator;
         private Vector2 _inputDirection;
         private Vector2 _lastDirection;
         private Vector2 _lastInput;
         private Rigidbody2D _rigidbody2D;
+        private Vector2 _spawnPosition;
+
+        public Vector2 Destination { get; set; }
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _destination = (Vector2)transform.position + _orignalDirection * initPositionOffset;
-            _lastDirection = _orignalDirection;
+            var position = transform.position;
+            Destination = (Vector2)position + originalDirection * initPositionOffset;
+            _spawnPosition = position;
+            _lastDirection = originalDirection;
+            RotateRenderer();
         }
 
         private void FixedUpdate()
         {
             var position = transform.position;
-            var positionVector = Vector2.MoveTowards(position, _destination, speed * Time.deltaTime);
+            var positionVector = Vector2.MoveTowards(position, Destination, speed * Time.deltaTime);
             _rigidbody2D.MovePosition(positionVector);
 
-            var isCentered = (Vector2)position == _destination;
+            var isCentered = (Vector2)position == Destination;
 
             if (!isCentered) return;
 
             // if is at the middle of a tile, has input and there is no wall in the direction of the input
             if (_lastInput != Vector2.zero && !DetectWallBorder(_lastInput))
             {
-                _destination = (Vector2)transform.position + _lastInput;
+                Destination = (Vector2)transform.position + _lastInput;
                 _lastDirection = _lastInput;
+                animator.SetBool("isMoving", true);
+                RotateRenderer();
             }
             // if is at the middle of a tile and there is no wall in the current direction then continue in the same direction
             else if (!DetectWallBorder(_lastDirection))
             {
-                _destination = (Vector2)transform.position + _lastDirection;
+                Destination = (Vector2)transform.position + _lastDirection;
+                animator.SetBool("isMoving", true);
             }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+
+        private void RotateRenderer()
+        {
+            var angle = Vector2.SignedAngle(Vector2.right, _lastDirection);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
         }
 
         private bool DetectWallBorder(Vector3 dir)
@@ -58,7 +78,11 @@ namespace Player
         private void OnMove(InputValue value)
         {
             _inputDirection = value.Get<Vector2>();
-            if (_inputDirection != Vector2.zero) _lastInput = _inputDirection;
+
+            if (_inputDirection.x != 0) _inputDirection.y = 0; // Create a priority for x movement
+
+            if (_inputDirection != Vector2.zero)
+                _lastInput = _inputDirection.normalized; // Normalize the output to be 1 or -1 not floating values
         }
     }
 }
--- Assets/Scripts/Teleporter/Teleporter.cs
+++ Assets/Scripts/Teleporter/Teleporter.cs
@@ -1,11 +1,21 @@
+using Player;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
     public GameObject target;
+    public string playerTag = "Player";
+    public string enemyTag = "Enemy";
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         other.GetComponent<Transform>().position = target.GetComponent<Transform>().position;
+
+        if (other.gameObject.CompareTag(playerTag))
+            other.gameObject.GetComponent<PlayerController>().Destination =
+                target.GetComponent<Transform>().position;
+        // else if (other.gameObject.CompareTag(enemyTag))
+        //     other.gameObject.GetComponent<EnemyController>().Destination =
+        //         target.GetComponent<Transform>().position;
     }
 }
